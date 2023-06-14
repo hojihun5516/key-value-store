d01c2b54d4683965bd34852751749a00919dc568
moflow <hojihun5516@daum.net>
2023-06-14 21:02:41 +0900

    [ConsistenceHashMap] key가 저장될 VirtualNode를 가져온다
    * virtual node를 가져오는데 key를 해시한 값보다 첫번째로 큰 Virtual Node 값을 가져온다
    * 만약 key를 해시한 값보다 큰 값을 가진 VirtualNode가 없다면 첫번째 Virtual Node를 가져온다
    
    close:#11

--- src/test/kotlin/com/example/keyvaluestore/store/ConsistenceHashMapTest.kt
+++ src/test/kotlin/com/example/keyvaluestore/store/ConsistenceHashMapTest.kt
@@ -31,4 +31,42 @@ class ConsistenceHashMapTest(
         // Assert
         assertThat(circle.size).isEqualTo(100)
     }
+
+    @Test
+    @DisplayName("key가 주어졌을 때 key의 해시값보다 큰 virtual node를 리턴한다")
+    fun `sut should get correct virtual node when key is given`() {
+        // Arrange
+        every { hash.hash(any()) } returnsMany List(100) { Random().nextLong() }
+        val sut = ConsistenceHashMap(hash)
+
+        val key = "testKey"
+        val hashValue: Long = 1234 // example hash value
+        every { hash.hash(key) } returns hashValue
+
+        // Act
+        val virtualNode = sut.getVirtualNode(key)
+
+        // Assert
+        assertThat(virtualNode).isNotNull
+        assertThat(virtualNode.hash).isGreaterThanOrEqualTo(hashValue)
+    }
+
+    @Test
+    @DisplayName("key가 주어졌을 때 key의 해시값보다 큰 virtual noder가 없다면 첫번째 virtual node를 리턴한다")
+    fun `sut should return first virtual node when correct virtual node is not exists`() {
+        // Arrange
+        every { hash.hash(any()) } returnsMany List(100) { Random().nextLong() }
+        val sut = ConsistenceHashMap(hash)
+
+        val key = "testKey"
+        val hashValue: Long = Long.MAX_VALUE // example hash value
+        every { hash.hash(key) } returns hashValue
+
+        // Act
+        val virtualNode = sut.getVirtualNode(key)
+
+        // Assert
+        assertThat(virtualNode).isNotNull
+        assertThat(virtualNode.hash).isLessThan(hashValue)
+    }
 }
