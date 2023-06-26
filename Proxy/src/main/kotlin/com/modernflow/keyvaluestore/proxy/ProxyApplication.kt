4f18ea0f0470beab2e55928a765d1ff137ebf20e
moflow <hojihun5516@daum.net>
2023-06-26 19:55:21 +0900

    [Rename] 프로젝트 이름 수정 -> proxy
    close: #21

--- Proxy/src/main/kotlin/com/modernflow/keyvaluestore/proxy/ProxyApplication.kt
+++ Proxy/src/main/kotlin/com/modernflow/keyvaluestore/proxy/ProxyApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KeyValueStoreApplication
+class ProxyApplication
 
 fun main(args: Array<String>) {
-	runApplication<KeyValueStoreApplication>(*args)
+	runApplication<ProxyApplication>(*args)
 }
--- Proxy/src/test/kotlin/com/modernflow/keyvaluestore/proxy/ProxyApplicationTests.kt
+++ Proxy/src/test/kotlin/com/modernflow/keyvaluestore/proxy/ProxyApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class KeyValueStoreApplicationTests {
+class ProxyApplicationTests {
 
 	@Test
 	fun contextLoads() {
