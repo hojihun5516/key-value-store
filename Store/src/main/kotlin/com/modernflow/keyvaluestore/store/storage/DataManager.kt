5123feb97f26b8b312f69198ad0dc1edc3a70a7b
moflow <hojihun5516@daum.net>
2023-07-01 18:15:55 +0900

    [Store] 리턴타입변경
    close: #29

--- Store/src/main/kotlin/com/modernflow/keyvaluestore/store/storage/DataManager.kt
+++ Store/src/main/kotlin/com/modernflow/keyvaluestore/store/storage/DataManager.kt
@@ -1,9 +1,9 @@
 package com.modernflow.keyvaluestore.store.storage
 
 interface DataManager {
-    fun upsert(key: Long, value: Any)
+    fun upsert(key: Long, value: Any): Boolean
 
     fun get(key: Long): Any?
 
-    fun delete(key: Long)
+    fun delete(key: Long): Boolean
 }
