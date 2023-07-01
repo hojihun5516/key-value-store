484bc8d55485ed34dca81aaa48d11ab4b321cab4
moflow <hojihun5516@daum.net>
2023-07-01 18:15:55 +0900

    [Store] 리턴타입변경
    close: #29

--- Store/src/main/kotlin/com/modernflow/keyvaluestore/store/storage/InMemoryDataManager.kt
+++ Store/src/main/kotlin/com/modernflow/keyvaluestore/store/storage/InMemoryDataManager.kt
@@ -7,15 +7,17 @@ import java.util.concurrent.ConcurrentHashMap
 class InMemoryDataManager : DataManager {
     private val dataMap: MutableMap<Long, Any> = ConcurrentHashMap<Long, Any>()
 
-    override fun upsert(key: Long, value: Any) {
+    override fun upsert(key: Long, value: Any): Boolean {
         dataMap[key] = value
+        return true
     }
 
     override fun get(key: Long): Any? {
         return this.dataMap[key]
     }
 
-    override fun delete(key: Long) {
+    override fun delete(key: Long): Boolean {
         dataMap.remove(key)
+        return true
     }
 }
