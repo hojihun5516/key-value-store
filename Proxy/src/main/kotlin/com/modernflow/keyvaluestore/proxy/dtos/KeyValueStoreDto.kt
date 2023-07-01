c48f74af045fb5bb286f28f5291364d55382be54
moflow <hojihun5516@daum.net>
2023-07-01 18:18:35 +0900

    [KeyValueStoreDto] value 타입 변경
    close: #33

--- Proxy/src/main/kotlin/com/modernflow/keyvaluestore/proxy/dtos/KeyValueStoreDto.kt
+++ Proxy/src/main/kotlin/com/modernflow/keyvaluestore/proxy/dtos/KeyValueStoreDto.kt
@@ -2,5 +2,5 @@ package com.modernflow.keyvaluestore.proxy.dtos
 
 data class KeyValueStoreDto(
     val key: String,
-    val value: String,
+    val value: Any,
 )
---
+++ Store/src/main/kotlin/com/modernflow/keyvaluestore/store/dtos/KeyValueStoreDto.kt
@@ -0,0 +1,6 @@
+package com.modernflow.keyvaluestore.store.dtos
+
+data class KeyValueStoreDto(
+    val key: Long,
+    val value: Any,
+)
