c6f86e44739f6b0cdae39a0f11f2cb3b4211d34b
moflow <hojihun5516@daum.net>
2023-07-03 21:21:36 +0900

    [PhysicalAddressClientService] 생성
    * ip와 port정보를 받아서 해당하는 클라이언트 정보를 리턴한다
    
    close: #44

---
+++ ServiceDiscovery/src/test/kotlin/com/modernflow/keyvaluestore/servicediscovery/services/PhysicalAddressClientServiceTest.kt
@@ -0,0 +1,65 @@
+package com.modernflow.keyvaluestore.servicediscovery.services
+
+import com.modernflow.keyvaluestore.servicediscovery.addresses.PhysicalAddress.FIRST_PHYSICAL_NODE
+import com.modernflow.keyvaluestore.servicediscovery.addresses.PhysicalAddress.SECOND_PHYSICAL_NODE
+import com.modernflow.keyvaluestore.servicediscovery.addresses.PhysicalAddress.THIRD_PHYSICAL_NODE
+import com.modernflow.keyvaluestore.servicediscovery.clients.FirstStoreClient
+import com.modernflow.keyvaluestore.servicediscovery.clients.SecondStoreClient
+import com.modernflow.keyvaluestore.servicediscovery.clients.ThirdStoreClient
+import com.modernflow.keyvaluestore.servicediscovery.dtos.PhysicalNodeAddressDto
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito.mock
+
+@ExtendWith(MockKExtension::class)
+class PhysicalAddressClientServiceTest(
+    @MockK private val firstStoreClient: FirstStoreClient,
+    @MockK private val secondStoreClient: SecondStoreClient,
+    @MockK private val thirdStoreClient: ThirdStoreClient,
+) {
+    @InjectMockKs
+    private lateinit var sut: PhysicalAddressClientService
+
+    @Test
+    fun `sut should return storeClient when target physical node is given`() {
+        // Arrange
+        val firstPhysicalNode = FIRST_PHYSICAL_NODE
+        val secondPhysicalNode = SECOND_PHYSICAL_NODE
+        val thirdPhysicalNode = THIRD_PHYSICAL_NODE
+
+        // Act
+        val firstStoreClientResult = sut.getStoreClient(firstPhysicalNode)
+        val secondStoreClientResult = sut.getStoreClient(secondPhysicalNode)
+        val thirdStoreClientResult = sut.getStoreClient(thirdPhysicalNode)
+
+        // Assert
+        assertEquals(firstStoreClient, firstStoreClientResult)
+        assertEquals(secondStoreClient, secondStoreClientResult)
+        assertEquals(thirdStoreClient, thirdStoreClientResult)
+    }
+
+    @Test
+    fun `sut throw exception when PhysicalNodeAddressDto is not found`() {
+        // Arrange
+        val firstStoreClient = mock(FirstStoreClient::class.java)
+        val secondStoreClient = mock(SecondStoreClient::class.java)
+        val thirdStoreClient = mock(ThirdStoreClient::class.java)
+        val physicalAddressClientService = PhysicalAddressClientService(
+            firstStoreClient,
+            secondStoreClient,
+            thirdStoreClient
+        )
+
+        val unknownPhysicalNode = PhysicalNodeAddressDto("unknownIp", 0)
+
+        // Act & Assert
+        assertThrows(Exception::class.java) {
+            physicalAddressClientService.getStoreClient(unknownPhysicalNode)
+        }
+    }
+}
