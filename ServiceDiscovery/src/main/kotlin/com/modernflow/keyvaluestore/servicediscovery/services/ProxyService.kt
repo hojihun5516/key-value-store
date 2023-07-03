c697a5368ed1f496279c94a1a880334347248b9d
moflow <hojihun5516@daum.net>
2023-07-03 21:19:08 +0900

    [ProxyServer] Proxy 서버에 비동기로 해쉬링에 기록되어 있는 physical Store 서버를 제거 요청
    close: #43

---
+++ ServiceDiscovery/src/main/kotlin/com/modernflow/keyvaluestore/servicediscovery/services/ProxyService.kt
@@ -0,0 +1,32 @@
+package com.modernflow.keyvaluestore.servicediscovery.services
+
+import com.modernflow.keyvaluestore.servicediscovery.dtos.PhysicalNodeAddressDto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.RestTemplate
+
+@Service
+class ProxyService(private val restTemplate: RestTemplate) {
+    suspend fun removeStoreServer(physicalNodeAddressDto: PhysicalNodeAddressDto) {
+        val (ip, port) = physicalNodeAddressDto
+        val removeUrl = "http://localhost:8000/consistence-hash-ring/ip/$ip/port/$port"
+
+        try {
+            withContext(Dispatchers.IO) {
+                restTemplate.delete(removeUrl)
+            }
+            println("A 스토어 서버 제거 요청이 성공적으로 완료되었습니다.")
+        } catch (ex: HttpClientErrorException) {
+            if (ex.statusCode == HttpStatus.NOT_FOUND) {
+                println("A 스토어 서버를 찾을 수 없습니다.")
+            } else {
+                println("A 스토어 서버 제거 요청이 실패하였습니다. 응답코드: ${ex.statusCode.value()}")
+            }
+        } catch (ex: Exception) {
+            println("A 스토어 서버 제거 요청 중에 예외가 발생하였습니다: ${ex.message}")
+        }
+    }
+}
