b32857375e45d95590fa4a0c7125dcf4756cef09
moflow <hojihun5516@daum.net>
2023-08-08 19:31:52 +0900

    [ConsistenceHashMapController] 해시맵에 관련된 컨트롤러 추가
    * 기존 InitHashMapController를 ConsistenceHashMapController로 변경한다
    * 해시링에서 특정 physicalNode를 지우는 endpoint를 추가한다
    
    close: #75

--- Proxy/src/main/kotlin/com/modernflow/keyvaluestore/proxy/controllers/ConsistenceHashMapController.kt
+++ Proxy/src/main/kotlin/com/modernflow/keyvaluestore/proxy/controllers/ConsistenceHashMapController.kt
@@ -1,23 +1,36 @@
 package com.modernflow.keyvaluestore.proxy.controllers
 
 import com.modernflow.keyvaluestore.clients.ServiceDiscoveryClient
+import com.modernflow.keyvaluestore.dtos.PhysicalNodeAddressDto
 import com.modernflow.keyvaluestore.proxy.store.ConsistenceHashMap
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class InitController(
+class ConsistenceHashMapController(
     private val consistenceHashMap: ConsistenceHashMap,
     private val serviceDiscoveryClient: ServiceDiscoveryClient,
 ) {
     @PostMapping(
-        "/proxy/init-consistence-hash-map",
+        "/proxy/consistence-hash-map/init",
         consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE],
     )
     fun init(): Boolean {
         val storeAddresses = serviceDiscoveryClient.getStoreAddress()
         return consistenceHashMap.createCirCle(storeAddresses)
+    }
+
+    @DeleteMapping("/proxy/consistence-hash-map/ip/{ip}/port/{port}")
+    fun delete(
+        @PathVariable ip: String,
+        @PathVariable port: Int,
+    ) {
+        consistenceHashMap.removePhysicalNode(
+            PhysicalNodeAddressDto(ip = ip, port = port)
+        )
     }
 }
