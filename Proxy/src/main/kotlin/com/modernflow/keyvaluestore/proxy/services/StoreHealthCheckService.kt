abf0a6adc565eab3e7c410539e4152c841ec028d
moflow <hojihun5516@daum.net>
2023-08-08 20:05:48 +0900

    [Logging] 로깅 추가 및 println 대체
    close: #81

--- Proxy/src/main/kotlin/com/modernflow/keyvaluestore/proxy/services/StoreHealthCheckService.kt
+++ Proxy/src/main/kotlin/com/modernflow/keyvaluestore/proxy/services/StoreHealthCheckService.kt
@@ -3,7 +3,11 @@ package com.modernflow.keyvaluestore.proxy.services
 import com.modernflow.keyvaluestore.clients.ServiceDiscoveryClient
 import com.modernflow.keyvaluestore.dtos.PhysicalNodeAddressDto
 import com.modernflow.keyvaluestore.proxy.store.ConsistenceHashMap
+import feign.FeignException
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Service
+
+private val logger = KotlinLogging.logger {}
 
 @Service
 class StoreHealthCheckService(
@@ -13,7 +17,18 @@ class StoreHealthCheckService(
     fun isStoreHealth(key: String): Pair<Boolean, PhysicalNodeAddressDto> {
         val (_, hash, physicalNode) = consistenceHashMap.getVirtualNode(key)
         val (ip, port) = physicalNode
-        val isStoreHealth = serviceDiscoveryClient.isHealthAddress(ip, port)
-        return Pair(isStoreHealth, physicalNode)
+        return try {
+            val isStoreHealth = serviceDiscoveryClient.isHealthAddress(ip, port)
+            Pair(isStoreHealth, physicalNode)
+        } catch (ex: FeignException.InternalServerError) {
+            logger.error { "isStoreHealth - InternalServerError 발생 key: $key, physicalNode: $physicalNode, message: ${ex.message}" }
+            return Pair(false, physicalNode)
+        } catch (ex: java.net.UnknownHostException) {
+            logger.error { "isStoreHealth - UnknownHostException 발생 key: $key, physicalNode: $physicalNode, message: ${ex.message}" }
+            return Pair(false, physicalNode)
+        } catch (ex: Exception) {
+            logger.error { "isStoreHealth - Exception 발생 key: $key, physicalNode: $physicalNode, message: ${ex.message}" }
+            return Pair(false, physicalNode)
+        }
     }
 }
