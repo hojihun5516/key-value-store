5a54c8f382881c0c15c40ade7b76fe402a8832d5
moflow <hojihun5516@daum.net>
2023-08-08 19:26:47 +0900

    [ConsistenceHashMap] removePhysicalNode추가
    * 해시링에서 물리노드를 제거하는 함수를 추가한다
    
    close: #76

--- Proxy/src/test/kotlin/com/modernflow/keyvaluestore/proxy/store/ConsistenceHashMapTest.kt
+++ Proxy/src/test/kotlin/com/modernflow/keyvaluestore/proxy/store/ConsistenceHashMapTest.kt
@@ -7,6 +7,7 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -76,6 +77,27 @@ class ConsistenceHashMapTest(
         // Assert
         assertThat(virtualNode).isNotNull
         assertThat(virtualNode.physicalNode).isEqualTo(expectedPhysicalNode)
+    }
+
+    @Test
+    @DisplayName("제거할 physicalNode 정보를 받아서 해당하는 가상노드를 전부 제거한다")
+    fun `sut should remove phyiscalNode on circle`() {
+        // Arrange
+        every { hash.hash(any()) } returnsMany List(300) { Random().nextLong() }
+        val sut = ConsistenceHashMap(hash)
+        sut.createCirCle(getPhysicalAddresses())
+        val removedTargetPhysicalNode = PhysicalNodeAddressDto(
+            ip = "store-service-1",
+            port = 5000,
+        )
+
+        // Act
+        sut.removePhysicalNode(removedTargetPhysicalNode)
+
+        // Assert
+        val circle = ReflectionTestUtils.getField(sut, "circle") as SortedMap<Long, VirtualNode>
+
+        assertFalse(circle.values.any { it.physicalNode == removedTargetPhysicalNode })
     }
 
     private fun getPhysicalAddresses(): List<PhysicalNodeAddressDto> {
