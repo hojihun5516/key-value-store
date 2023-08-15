9618d04759d6ce046016a46337afa7163f7bc0b8
moflow <hojihun5516@daum.net>
2023-08-15 23:41:52 +0900

    [Store] Redis에 store 서비스의 heartbeat 카운트를 추가한다
    * 가십프로토콜을 위해 Store 서비스가 redis에 heartBeat Count를 1씩 증가시킨다
    
    work-items: #90

--- Store/src/main/kotlin/com/modernflow/keyvaluestore/store/StoreApplication.kt
+++ Store/src/main/kotlin/com/modernflow/keyvaluestore/store/StoreApplication.kt
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @ComponentScan(basePackages = ["com.modernflow.keyvaluestore"])
 @EnableFeignClients(basePackages = ["com.modernflow"])
+@EnableScheduling
 class StoreApplication
 
 fun main(args: Array<String>) {
