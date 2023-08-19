2b850e06a8a8ecb94b97227c6ac1ecffcf5f8bfd
moflow <hojihun5516@daum.net>
2023-08-19 19:26:02 +0900

    [Proxy] ConsistenceHashMap init 방식 변경
    * 서버가 실행되는 순간에 하드코딩되어있는 Store 목록을 사용하여 HashMap을 Init 한다
    
    close: #99

--- Proxy/src/main/kotlin/com/modernflow/keyvaluestore/proxy/controllers/ConsistenceHashMapController.kt
+++ Proxy/src/main/kotlin/com/modernflow/keyvaluestore/proxy/controllers/ConsistenceHashMapController.kt
@@ -1,36 +1,24 @@
 package com.modernflow.keyvaluestore.proxy.controllers
 
-import com.modernflow.keyvaluestore.clients.ServiceDiscoveryClient
 import com.modernflow.keyvaluestore.dtos.PhysicalNodeAddressDto
 import com.modernflow.keyvaluestore.proxy.store.ConsistenceHashMap
-import org.springframework.http.MediaType
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+
+private val logger = KotlinLogging.logger {}
 
 @RestController
 class ConsistenceHashMapController(
     private val consistenceHashMap: ConsistenceHashMap,
-    private val serviceDiscoveryClient: ServiceDiscoveryClient,
 ) {
-    @PostMapping(
-        "/proxy/consistence-hash-map/init",
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE],
-    )
-    fun init(): Boolean {
-        val storeAddresses = serviceDiscoveryClient.getStoreAddress()
-        return consistenceHashMap.createCirCle(storeAddresses)
-    }
-
     @DeleteMapping("/proxy/consistence-hash-map/ip/{ip}/port/{port}")
     fun delete(
         @PathVariable ip: String,
         @PathVariable port: Int,
     ) {
-        consistenceHashMap.removePhysicalNode(
-            PhysicalNodeAddressDto(ip = ip, port = port)
-        )
+        logger.info { "remove physical node request - $ip, $port" }
+        consistenceHashMap.removePhysicalNode(PhysicalNodeAddressDto(ip = ip, port = port))
     }
 }
